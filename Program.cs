6af5405c84e8ba55490c23a6c55aca0dab2b4832
lucas <lucasvchacon1020@gmail.com>
2021-07-29 12:29:46 -0300

    Aula 1 Alura Primeiros Passoss - PP

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AluraPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int idade = 32;
+            Console.WriteLine("sua idade é: " +idade);
+            //Console.WriteLine(idade);
+
+
+            Console.WriteLine("Programa 1");
+            Console.WriteLine( "A Execução acabou tecle enter pra continuar...2");
+
+            Console.ReadLine();
+       }
+    }
+}
